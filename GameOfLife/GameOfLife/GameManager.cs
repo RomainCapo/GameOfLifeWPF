fcdf8e349dc27631407bb5ca4ef3ef6cd1994680
Vincent Moulin <vincent.moulin@he-arc.ch>
2019-11-11 20:59:07 +0100

    edit UI

---
+++ GameOfLife/GameOfLife/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class GameManager
+    {
+        private int width;
+        private int height;
+        
+        public GameManager(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        public int Width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }
+
+        public int Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+    }
+}
